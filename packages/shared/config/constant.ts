d62405327a1fa89846a497bf966c3e7c32df0f98
zetaloop <zetaloop@outlook.com>
2025-12-30 07:03:34 +0800

    fix: 增加模型超时时间至30分钟

--- packages/shared/config/constant.ts
+++ packages/shared/config/constant.ts
@@ -218,7 +218,7 @@ export enum UpdateMirror {
   GITCODE = 'gitcode'
 }
 
-export const defaultTimeout = 10 * 1000 * 60
+export const defaultTimeout = 30 * 1000 * 60
 
 export const occupiedDirs = ['logs', 'Network', 'Partitions/webview/Network']
 
